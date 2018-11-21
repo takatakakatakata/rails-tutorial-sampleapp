42e721ad9bcaa91ac3f39474958824dabf053c03
takahirohayasaka <>
2018-11-22 02:06:11 +0900

    hello world の印字

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "¡hola mundo!"
+  end
+
+  def goodbye
+    render html: "good bye world!"
+  end
+
 end
